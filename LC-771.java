6d59a4f70c399d2b833db7a1faa7737c5a2266e2
GobbarakalluSurendra <Sgobbarakallu@gmail.com>
2025-09-02 23:11:24 +0530

    DSA Problem

---
+++ LC-771.java
@@ -0,0 +1,23 @@
+class Solution {
+    public static int numJewelsInStones(String jewels, String stones) {
+        int n = jewels.length();
+        int m = stones.length();
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            char ch = jewels.charAt(i);
+            for (int j = 0; j < m; j++) {
+                char ch1 = stones.charAt(j);
+                if (ch == ch1)
+                    count++;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        String jewels = "aA";
+        String stones = "aAAbbbb";
+
+        System.out.println(numJewelsInStones(jewels, stones)); // ✅ directly call
+    }
+}
