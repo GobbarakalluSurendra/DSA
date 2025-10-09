6ee669a21f35e3189e57ee2993fcc150ed3cf171
GobbarakalluSurendra <Sgobbarakallu@gmail.com>
2025-10-09 19:16:57 +0530

    LC-1512

--- LC-1512.java
+++ LC-1512.java
@@ -54,29 +54,3 @@
 // }
 
 
-//USING HashMap with Maths
-import java.util.*;
-
-class Solution {
-    public int numIdenticalPairs(int[] nums) {
-        HashMap<Integer,Integer> hm = new HashMap<>();
-        int ans = 0;
-
-        for (int n : nums) {
-            if (hm.containsKey(n)) {
-                ans += hm.get(n);
-                hm.put(n, hm.get(n) + 1);
-            } else {
-                hm.put(n, 1);
-            }
-        }
-
-        return ans;
-    }
-
-    public static void main(String[] args) {
-        Solution sol = new Solution();
-        int nums[] = {1, 2, 3, 1, 1, 3};
-        System.out.println(sol.numIdenticalPairs(nums));  // Output: 4
-    }
-}
