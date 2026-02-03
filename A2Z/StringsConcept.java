a1c1ffc328203fc65d7b2193ad1b225d65244b58
GobbarakalluSurendra <Sgobbarakallu@gmail.com>
2026-02-03 18:05:02 +0530

    Strings Concept

---
+++ A2Z/StringsConcept.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class Solution{
+    public static void main(String args[]){
+        Scanner Sc=new Scanner(System.in);
+        // String Firstname="Surendra";
+        // String lastname=Sc.nextLine();
+        // if(Firstname.compareTo(lastname)==0){
+        //     System.out.println("Strings are Equal");
+
+        // }
+        // else{
+        //     System.out.println("Strings Are not Equal");
+        // }
+        // System.out.println(Firstname+" "+lastname);
+        // System.out.println("length:"+Firstname.length());
+        // for(int i=0;i<Firstname.length();i++){
+        //     System.out.println(Firstname.charAt(i));
+        // }
+        String Sentence="My Name is Surendra";
+        String name=Sentence.substring(10,Sentence.length());
+        System.out.println(name);
+
+        Sc.close();
+    }
+}
