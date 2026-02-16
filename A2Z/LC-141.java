c61ca57dd446671955c46128eeec4dfc79102ecf
GobbarakalluSurendra <Sgobbarakallu@gmail.com>
2026-02-16 18:35:26 +0530

    LC-141.java

---
+++ A2Z/LC-141.java
@@ -0,0 +1,54 @@
+//Using HashMAp
+import java.util.*;
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        HashMap<ListNode,Integer> hs=new HashMap<>();
+        ListNode temp=head;
+
+        while(temp!=null){
+            if(hs.containsKey(temp)){
+                return true;
+            }
+            hs.put(temp,1);
+            temp=temp.next;
+        }
+        return false;
+        
+    }
+}
+
+//Using HashSet
+import java.util.*;
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        HashSet<ListNode> hs=new HashSet<>();
+        ListNode temp=head;
+
+        while(temp!=null){
+            if(hs.contains(temp)){
+                return true;
+            }
+            hs.add(temp);
+            temp=temp.next;
+        }
+        return false;
+        
+    }
+}
+
+//Using TortoiseHare Method
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+       
+       ListNode fast=head;
+       ListNode slow=head;
+       while(fast !=null && fast.next!=null){
+        slow=slow.next;
+        fast=fast.next.next;
+        if(slow==fast){
+            return true;
+        }
+       }
+        return false;
+    }
+}
