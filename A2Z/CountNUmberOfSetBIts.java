2bec6b994a5f4819057f999764d368b674139186
GobbarakalluSurendra <Sgobbarakallu@gmail.com>
2026-03-04 18:52:30 +0530

    Count NUmber Of SetBits

---
+++ A2Z/CountNUmberOfSetBIts.java
@@ -0,0 +1,11 @@
+class Main {
+    public static void main(String[] args) {
+    int n=9;
+   int count=0;
+   while(n!=0){
+       n=(n & (n-1));
+       count++;
+   }
+   System.out.println(count);
+    }
+}
