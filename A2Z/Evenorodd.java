dbdc5517efa6a7744a2f35e04a46f936fdfe6335
GobbarakalluSurendra <Sgobbarakallu@gmail.com>
2026-03-04 18:44:02 +0530

    EvenOrOdd

--- A2Z/Evenorodd.java
+++ A2Z/Evenorodd.java
@@ -9,3 +9,16 @@ class Solution {
         
     }
 }
+
+// /Second Method
+class Main {
+    public static void main(String[] args) {
+    int a=9;
+    if((a & 1)==0){
+        System.out.println("Even");
+    }
+    else{
+        System.out.println("Odd");
+    }
+    }
+}
