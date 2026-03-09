79636852b3ffe86864e33a8be82a17a6196d08db
GobbarakalluSurendra <Sgobbarakallu@gmail.com>
2026-03-09 17:41:52 +0530

    Leetcode-136

--- A2Z/LC-136.java
+++ A2Z/LC-136.java
@@ -11,3 +11,14 @@ class Solution {
         return nums[nums.length - 1];
     }
 }
+
+//Second Method
+class Solution {
+    public int singleNumber(int[] nums) {
+       int xor=0;
+       for(int i:nums){
+        xor ^=i;
+       }
+       return xor;
+    }
+}
